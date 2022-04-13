ca401fc35488fd3481f63c0758a4651619eb48e2
unudeveloper <unudeveloper@gmail.com>
2022-04-13 12:16:23 -0500

    added routing and basic web app scaffolding

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+//import React, { useEffect } from 'react';
 import './App.css';
+import {
+  BrowserRouter,
+  Navigate,
+  Route,
+  Routes,
+} from "react-router-dom";
+import LaunchGame from './components/game/LaunchGame';
+import Home from './components/Home';
+import About from './components/About';
+import { MoralisProvider } from "react-moralis";
+
+function UserProfile() {
+  return <h1>User Profile! shows your NFTs here</h1>
+}
+
+
+function Mint() {
+  return <h1>Minting page</h1>
+}
+
+
+function NotFound() {
+  return <h1>not found!</h1>
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <MoralisProvider serverUrl="https://u4usrl90abnw.usemoralis.com:2053/server" appId="pO4GGOpd6Qu3ampCQ5f4r2nS8WadcC8bv9q9V3PG">
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about-team" element={<About />} />
+            <Route path="profiles/:address" element={<UserProfile />} />
+            <Route path="/mint" element={<Mint />} />
+            <Route path="game" element={<LaunchGame />} />
+            <Route path="/NotFound" element={<NotFound />} />
+            <Route path="*" element={<Navigate replace to="/NotFound" />} />          </Routes>
+        </div>
+      </MoralisProvider>
+    </BrowserRouter>
   );
 }
 
