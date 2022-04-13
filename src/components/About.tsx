b340a5bf570e7dced29a9764ddf1c9bb5e027b99
unudeveloper <unudeveloper@gmail.com>
2022-04-13 12:55:22 -0500

    scaffolding for various sections of web dapp

---
+++ src/components/About.tsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+
+
+export default function About() {
+    return <>
+        
+        <h2>About Us</h2>
+        <ul>
+            <li>Jack Bohm</li>
+            <li>Jovan Jester</li>
+        </ul>
+    
+    </>;
+
+}
---
+++ src/components/Header.tsx
@@ -0,0 +1,55 @@
+import { Link } from "react-router-dom";
+import Navbar from "./Navbar";
+import { useMoralis, useMoralisWeb3Api } from "react-moralis";
+import { useEffect, useState } from "react";
+
+export default function Header() {
+    const [walletAddress, setWalletAddress] = useState("");
+    const [ensName, setEnsName] = useState("");
+
+    const { authenticate, isAuthenticated, isAuthenticating, user, account, logout } = useMoralis();
+    const Web3Api = useMoralisWeb3Api();
+
+    const login = async () => {
+        if (!isAuthenticated) {
+
+            await authenticate({ signingMessage: "Sign this message in order to login." })
+                .then(function (user) {
+                    console.log("logged in user:", user);
+                    console.log(user!.get("ethAddress"));
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+        }
+    }
+
+    const logOut = async () => {
+        await logout();
+        console.log("logged out");
+    }
+
+    useEffect(() => {
+        const fetchEns = async (options: {address: string}) => {
+            // get ENS domain of an address
+            return await Web3Api.resolve.resolveAddress(options);
+        };
+
+        (async () => {
+            if (user !== null) {
+                setWalletAddress(user!.get("ethAddress"));
+                const ensName = await fetchEns({address: walletAddress});
+                setEnsName(ensName.name);
+            }
+        })();
+        
+    }, [user, Web3Api, ensName, walletAddress]);
+
+    return <><header className="App-header">
+        <h1>BlockChain Battle Arena</h1>
+        <Link className="address" to={"profiles/" + walletAddress}>{ensName || walletAddress}</Link>
+        <button className="button connect-wallet" onClick={isAuthenticated ? logOut : login}>{isAuthenticated ? "Disconnect" : "Connect Wallet"}</button>
+    </header>
+        <Navbar /></>
+}
+
