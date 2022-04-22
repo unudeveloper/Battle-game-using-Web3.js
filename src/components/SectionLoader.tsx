c53f70bee9ce7718e75a19f8e8344cf7cb6704b3
unudeveloper <unudeveloper@gmail.com>
2022-04-22 02:31:50 -0500

    switched from Moralis to plain ethers connect wallet

--- src/components/SectionLoader.tsx
+++ src/components/SectionLoader.tsx
@@ -10,75 +10,156 @@ import About from './About';
 import Mint from './Mint';
 import LaunchGame from '../game/LaunchGame';
 import useENSName from '../hooks/useENSName';
-import { getDefaultProvider } from 'ethers';
+import { ethers, getDefaultProvider } from 'ethers';
+import Moralis from 'moralis/types';
 
 export default function SectionLoader() {
   const { pathname } = useLocation();
-  
+
 
   const [walletAddress, setWalletAddress] = useState("");
-    const [ensName, setEnsName] = useState("");
+  const [ensName, setEnsName] = useState("");
+  const [errorMsg, setErrorMsg] = useState("");
+  const [isAuthenticating, setIsAuthenticating] = useState(false);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isChainCorrect, setIsChainCorrect] = useState(false);
+  const [currentAccount, setCurrentAccount] = useState("");
+  //const { authenticate, isAuthenticated, isAuthenticating, user, account, logout } = useMoralis();
+  //const Web3Api = useMoralisWeb3Api();
 
-    const { authenticate, isAuthenticated, isAuthenticating, user, account, logout } = useMoralis();
-    const Web3Api = useMoralisWeb3Api();
 
-    const login = async () => {
-        if (!isAuthenticated) {
 
-            await authenticate({ signingMessage: "Sign this message in order to login." })
-                .then(function (user: any) {
-                    console.log("logged in user:", user);
-                    console.log(user!.get("ethAddress"));
-                })
-                .catch(function (error: any) {
-                    console.log(error);
-                });
-        }
+
+
+  const checkChain = async () => {
+    try {
+      const { ethereum } = window as any;
+
+      if (!ethereum) {
+        setErrorMsg("The MetaMask wallet was not found. Please install it and try again.");
+        setIsAuthenticating(false);
+        return;
+      }
+
+      const provider = new ethers.providers.Web3Provider(ethereum, "any");
+      const { chainId } = await provider.getNetwork();
+      console.log("chainId: ", chainId);
+      if (chainId === 4) {
+        console.log("connected to Rinkeby testnet");
+        setErrorMsg("");
+        setIsChainCorrect(true);
+      } else {
+        setErrorMsg("Please connect to the Rinkeby testnet in MetaMask in order to continue.");
+        setIsChainCorrect(false);
+      }
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    const logOut = async () => {
-        await logout();
-        console.log("logged out");
+
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window as any;
+
+      if (!ethereum) {
+        setErrorMsg("The MetaMask wallet was not found. Please install it and try again.");
+        setIsAuthenticating(false);
+        return;
+      }
+
+      checkChain();
+      if (!isChainCorrect) {
+        setIsAuthenticating(false);
+        return;
+      }
+
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    useEffect(() => {
-        const mainnetProvider = getDefaultProvider(); // Moralis typescript types bug, switched to using ethers
-        if (walletAddress) {
-          mainnetProvider.lookupAddress(walletAddress).then((name) => {
-          if (typeof name === "string") {
-            setEnsName(name);
-            // update to actual ENS name if found
-          }
-        });
-        }
+  // const logOut = async () => {
+  //     await logout();
+  //     console.log("logged out");
+  // }
 
-        (async () => {
-            if (user !== null) {
-                setWalletAddress(user!.get("ethAddress"));
-                //const ensName = await fetchEns({address: walletAddress});
-                //setEnsName(ensName.name);
+  useEffect(() => {
+
+
+
+    const checkIfWalletIsConnected = async () => {
+      if (!isAuthenticated) {
+        try {
+          const { ethereum } = window as any;
+          setIsAuthenticating(true);
+          if (!ethereum) {
+            setErrorMsg("The MetaMask wallet was not found. Please install it and try again.");
+            setIsAuthenticating(false);
+            return;
+          } else {
+            const accounts = await ethereum.request({ method: "eth_accounts" });
+
+            if (accounts.length !== 0) {
+              const account = accounts[0];
+              console.log("Found authorized account:", account);
+              setCurrentAccount(account);
+              setIsAuthenticated(true);
+            } else {
+              console.log("No authorized account found");
             }
-        })();
+          }
 
-     }, [user, Web3Api, ensName, walletAddress]);
+          checkChain();
+          if (!isChainCorrect) {
+            setIsAuthenticating(false);
+            return;
+          }
+        } catch (error) {
+          console.log(error);
+        }
+        setIsAuthenticating(false);
+      }
+    }
+
+    const provider = getDefaultProvider(); // Moralis typescript types bug, switched to using ethers
+    if (currentAccount) {
+      provider.lookupAddress(currentAccount).then((name) => {
+        if (typeof name === "string") {
+          setEnsName(name);
+          // update to actual ENS name if found
+        }
+      });
+    }
+
+    checkIfWalletIsConnected();
+
+
+  }, [ensName, currentAccount]);
 
   return (<>
     <Header />
     <main className="main-container">
       <Navbar />
       {pathname === "/" && (<Home />)}
-      {pathname === "/mint" && (<Mint />)}
+      {pathname === "/mint" && (<Mint walletAddress={currentAccount} ensName={ensName} />)}
       {pathname === "/launch-game" && (<LaunchGame />)}
       {pathname === "/about" && (<About />)}
       {pathname === "/" && !isAuthenticated && (<ActionArea heading="Connect Wallet" message="" label={"Connect Wallet"} showButton={true}
-      disableButton={false} isLoading={false} smallButton={false} loadingLabel={""} action={() => login()} showLabel={true} />)}
-      {pathname === "/" && isAuthenticated && (<ActionArea heading="" message={"Connected as " + (ensName || walletAddress)} label={"Log Out"} showButton={true}
-      disableButton={false} isLoading={false} smallButton={true} loadingLabel={""} action={() => logOut()} showLabel={true} />)}
+        disableButton={false} isLoading={false} smallButton={false} loadingLabel={""} action={() => connectWallet()} showLabel={true} />)}
+      {pathname === "/" && isAuthenticated && (<ActionArea heading="" message={"Connected as " + (ensName || walletAddress)} label={""} showButton={false}
+        disableButton={false} isLoading={false} smallButton={true} loadingLabel={""} action={() => { }} showLabel={true} />)}
 
-        {pathname === "/about" && !isAuthenticated && (<ActionArea heading="Connect Wallet" message="" label={"Connect Wallet"} showButton={true}
-      disableButton={false} isLoading={false} smallButton={false} loadingLabel={""} action={() => login()} showLabel={true} />)}
+      {pathname === "/about" && !isAuthenticated && (<ActionArea heading="Connect Wallet" message="" label={"Connect Wallet"} showButton={true}
+        disableButton={false} isLoading={false} smallButton={false} loadingLabel={""} action={() => connectWallet()} showLabel={true} />)}
       {pathname === "/about" && isAuthenticated && (<ActionArea heading="" message={"Connected as " + (ensName || walletAddress)} label={"Log Out"} showButton={true}
-      disableButton={false} isLoading={false} smallButton={true} loadingLabel={""} action={() => logOut()} showLabel={true} />)}
+        disableButton={false} isLoading={false} smallButton={true} loadingLabel={""} action={() => connectWallet()} showLabel={true} />)}
     </main>
   </>
   );
