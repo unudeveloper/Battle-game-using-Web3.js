6f463f07d57b3ec7af2fc6d1728c7fe4d8d09c1c
unudeveloper <unudeveloper@gmail.com>
2022-05-05 16:08:25 -0500

    updated text in about and home sections

--- src/views/AboutView.tsx
+++ src/views/AboutView.tsx
@@ -18,20 +18,20 @@ export const AboutView = () => {
         </Paragraph>
         <SubHeading>Contributors</SubHeading>
         <UnorderedList>
-          <ListItem>StrictlyKappa</ListItem>
-          <ListItem>Jovan Jester</ListItem>
+          <ListItem>CYUBEART</ListItem>
           <ListItem>Rimraf</ListItem>
+          <ListItem>Jovan Jester</ListItem>
         </UnorderedList>
         <SubHeading>Tech Stack</SubHeading>
         <UnorderedList>
-          <ListItem>React/TypeScript</ListItem>
-          <ListItem>Kaboom game engine</ListItem>
-          <ListItem>Solidity on Rinkeby</ListItem>
+          <ListItem>Node with React/TypeScript</ListItem>
+          <ListItem>Kaboom</ListItem>
+          <ListItem>Solidity</ListItem>
         </UnorderedList>
         <SubHeading>Coming Soon</SubHeading>
         <UnorderedList>
           <ListItem>Multiplayer</ListItem>
-          <ListItem>Use your NFTs on the Ethereum mainnet</ListItem>
+          <ListItem>Use your NFTs from any EVM compatible mainnet, including Ethereum, of course, Polygon, KardiaChain, Harmony, Avalance and others</ListItem>
         </UnorderedList>
       </SectionContainer>
     </MainLayout>
--- src/views/HomeView.tsx
+++ src/views/HomeView.tsx
@@ -3,11 +3,9 @@ import { MainLayout } from '../components/Layout'
 import styled from 'styled-components'
 import {
   HeaderText,
-  ListItem,
   Paragraph,
   SectionContainer,
   SubHeading,
-  UnorderedList,
 } from '../components/shared'
 
 const HomeSectionContainer = styled(SectionContainer)`
@@ -34,40 +32,65 @@ export const HomeView = () => {
         <TextContainer>
           <Paragraph>
             <EmphasisText>Blockchain Battle Arena </EmphasisText>
-            is a battle game for your NFT characters. We have included a small
-            sample NFT collection with some exciting innovations beyond that of
-            the ERC721 NFT standard that is used by most profile picture NFT
-            collectibles today.
+            will be a cross-chain multiplayer battle game for NFTs.
+            Any standard NFT on most Ethereum Virtual Machine compatible blockchains
+            will be usable inside of the game. Your NFT characters will come to life
+            as they don a mech suit and battle each other using a variety of weapons.
+            The concept is loosely based on the Super Smash Brothers series of games,
+            but with a more Sci Fi aesthetic.
           </Paragraph>
-          <SubHeading>What's different about our NFTs?</SubHeading>
           <Paragraph>
-            Though if you mint a sample NFT it will show up on the testnets
-            OpenSea marketplace without seeming too different from other NFTs,
-            underneath the hood they have a few key differences:
+            The current demo is single player with a simple AI opponent and we only
+            have one battle stage, but the underpinnings are there for 4 to 6 player
+            multiplayer support, both local and over the internet. For local multiplayer,
+            this will be achieved through gamepad input support in addition to the keyboard.
+            Gamepads will be supported for online play as well, of course.
+          </Paragraph>
+          <SubHeading>Competition</SubHeading>
+          <Paragraph>
+            Though NFTs from different blockchains will be supported, we are still
+            figuring out the competition mechanics and how outcomes will be stored
+            and tracked. We would eventually like to have a leaderboard for both
+            individual players, tracked by wallet address, and for NFT collections,
+            tracked by contact address and chain id.
+          </Paragraph>
+          <Paragraph>
+            The initial version of this system will likely be on a traditional database
+            system while we figure out the specifics of the on-chain storage.
           </Paragraph>
         </TextContainer>
-        <UnorderedList>
-          <ListItem>
-            Accessories are separate NFTs that can be equipped on a character
-            NFT
-          </ListItem>
-          <ListItem>
-            The child/accesory NFTs can be attached to another NFT as long as
-            you are the owner or they can be listed on OpenSea in their own
-            right. Transferring or selling an accessory NFT will detach it from
-            its parent NFT.
-          </ListItem>
-          <ListItem>
-            This parent NFT can store positioning metadata onchain for all
-            accessory NFTs, thus allowing more flexibility in sharing
-            accessories between different styles of NFT collections
-          </ListItem>
-          <ListItem>
-            Finally, our proposal includes a method on the NFT smart contract to
-            get the token URI for a background free version of the NFT character
-            image, allowing drop in use in games or other web3 applications
-          </ListItem>
-        </UnorderedList>
+        <SubHeading>The Future of NFTs</SubHeading>
+        <Paragraph>
+          It seems NFT creators and collectors have become fixated on the 10k profile
+          picture collection model of distribution for NFTs. Though NFT collectibles are
+          fun to own and trade, we suspect the collectibles market is nearing saturation
+          and collectors are becoming fatigued by this model.
+        </Paragraph>
+        <Paragraph>
+          We would like to evolve the NFT standard to include the ability to mint and
+          trade accessories. This would mean collections could be made much bigger, not
+          just limited to 10k, and the monetization possibilities for creators would
+          increase as they can release seasonal accessory collections for their NFTs and
+          even usable by other collections as well. This would be a win for collectors as
+          well because it would allow greater customization and hence allow more creative
+          self expression.
+        </Paragraph>
+        <Paragraph>
+          In addition to accessories, we are also proposing evolving the NFT standard to
+          include methods in the smart contracts that provide the base NFT image with a
+          transparent background and perhaps a few simple different poses or expressions
+          for the character. These methods would unlock amazing interoperability with
+          different games and dApps from the get-go, instead of collectors having to wait
+          for the creators of their NFTs to launch a game or partner with other companies.
+        </Paragraph>
+        <Paragraph>
+          <EmphasisText>
+            In summary, we are imagining a future for digital collectibles on the blockchain
+            that provide instant utility from day one, by being customizable through
+            allowing the minting of accessories and immediate interoperability with a range
+            of web3 games and dApps in general.
+          </EmphasisText>
+        </Paragraph>
       </HomeSectionContainer>
     </MainLayout>
   )
